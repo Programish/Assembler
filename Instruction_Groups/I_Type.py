ed4e950454d06dc1a29f7f52e84460005c4b2894
Programish <harshit12kumar34@gmail.com>
2022-11-24 11:10:16 +0530

    Deal with negative numbers in I Type

--- Instruction_Groups/I_Type.py
+++ Instruction_Groups/I_Type.py
@@ -5,36 +5,54 @@ funct3 = {'JALR':'000', 'LB':'000', 'LH':'001', 'LW':'010', 'LBU':'100', 'LHU':'
           'ADDI':'000', 'SLTI':'010', 'SLTIU':'011', 'XORI':'100', 'ORI':'110', 'ANDI':'111'}
 
 def opcode(inst):
-    if inst[0].upper() is 'JALR':
+    if inst is 'JALR':
         return '1100111'
-    elif inst[0].upper() in ('LB', 'LH', 'LW', 'LBU', 'LHU'):
+    elif inst in ('LB', 'LH', 'LW', 'LBU', 'LHU'):
         return '0000011'
-    elif inst[0].upper() in ('ADDI', 'SLTI', 'SLTIU', 'XORI', 'ORI', 'ANDI'):
+    elif inst in ('ADDI', 'SLTI', 'SLTIU', 'XORI', 'ORI', 'ANDI'):
         return '0010011'
 
 def formatter(ins_lst):
     bin_res = '0b'
-    
-    if ins_lst[3].__contains__('x'):                    # Hex immediates
+    print() 
+    if ins_lst[3].__contains__('X'):                                # Hex immediates
         if len(bin(int(ins_lst[3], 16))[2:]) > 12:
             raise ValueError("Immediate passed is out of range!!!! \n Enter value <= 0xfff \n")
             return
         else:
-            bin_res += bin(int(ins_lst[3], 16))[2:]
-    elif ins_lst[3].__contains__('b'):                  # Bin immediates
+            bin_no = bin(int(ins_lst[3], 16))[2:]
+            fin_bin_no = '0' * (12-len(bin_no)) + bin_no
+            bin_res += fin_bin_no
+    elif ins_lst[3].__contains__('B'):                              # Bin immediates
         if len(ins_lst[3][2:]) > 12:
             raise ValueError("Immediate passed is out of range!!!! \n Enter value <= 0b111111111111 \n")
             return
         else:
-            bin_res += ins_lst[3][2:]
-    elif type(ins_lst[3]) is int:                       # Int immediates
-        if ins_lst[3] > 4095:
+            bin_no = ins_lst[3][2:]
+            fin_bin_no = '0' * (12-len(bin_no)) + bin_no
+            bin_res += fin_bin_no
+    elif ins_lst[3].isnumeric():                                    # Positive Int immediates
+        if int(ins_lst[3]) > 4095:
             raise ValueError("Immediate passed is out of range!!!! \n Enter value <= 4095 \n")
             return
         else:
-            bin_res += bin(ins_lst[3])[2:]
+            bin_no = bin(int(ins_lst[3]))[2:]
+            fin_bin_no = '0' * (12-len(bin_no)) + bin_no
+            bin_res += fin_bin_no
+
+    elif ins_lst[3][1:].isnumeric() and ins_lst[3][0] is '-':       # Negative Int immediates
+        if int(ins_lst[3][1:]) > 4094:
+            raise ValueError("Immediate passed is out of range!!!! \n Enter value >= -4094 \n")
+            return
+        else:
+            bin_no = bin(int(ins_lst[3][1:]))[2:]
+            fin_bin_no = '0' * (12-len(bin_no)) + bin_no
+            flipped_bits = ''.join(['1' if i == '0' else '0' for i in fin_bin_no])
+            bin_neg_no = bin(int(flipped_bits, 2) + int('1', 2))
+            bin_res += bin_neg_no[2:]
+
     else:
-        raise ValueError("Passed Immediate not allowed!!!! \n Only hex/bin/int values are allowed \n")
+        raise ValueError("Passed Immediate not allowed!!!! \nOnly hex/bin/int values are allowed \n")
         return
 
     if ins_lst[1].upper() in REG:
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import Instruction_Groups
-from Instruction_Groups import R_Type
+from Instruction_Groups import I_Type
 
 R = ['SLLI', 'SRLI', 'SRAI', 'ADD', 'SUB', 'SLL', 'SLT', 'SLTU', 'XOR', 'SRL', 'SRA', 'OR', 'AND']
 I = ['JALR', 'LB', 'LH', 'LW', 'LBU', 'LHU', 'ADDI', 'SLTI', 'SLTIU', 'XORI', 'ORI', 'ANDI']
@@ -11,6 +11,7 @@ J = ['JAL']
 s = ''
 while(s is not 'y'):
     s = input()
+    s = s.upper()
     ls = s.replace(",", "").split()
 #    flag = "1" if (ls[0][-1] in ("S", "s")) else "0"
 #    print("           OpCode : " + ls[0] + "    " + opcode[ls[0]])
@@ -19,11 +20,20 @@ while(s is not 'y'):
 #    print("  shifter operand : " + ls[3])
 #    print("      status flag : " + flag)
 #    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-    if ls[0].upper() in R:
-       R_Type.formatter(ls)
-    elif ls[0].upper() in J:
-       R_Type.formatter(ls)
-    elif ls[0].upper() in U:
-       R_Type.formatter(ls)
-
+    try:
+        if ls[0].upper() in R:
+            R_Type.formatter(ls)
+        elif ls[0].upper() in J:
+            J_Type.formatter(ls)
+        elif ls[0].upper() in U:
+            U_Type.formatter(ls)
+        elif ls[0].upper() in B:
+            B_Type.formatter(ls)
+        elif ls[0].upper() in I:
+            I_Type.formatter(ls)
+        elif ls[0].upper() in S:
+            S_Type.formatter(ls)
+    except ValueError as err:
+        print(str(err))
+    
     s = input("Want to quit?(y/n)")
