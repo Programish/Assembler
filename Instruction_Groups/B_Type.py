ff9f32b5b7dafb30498958b4bea8885d522769df
Programish <harshit12kumar34@gmail.com>
2022-12-04 11:13:40 +0530

    Fix bug for B and J type

--- Instruction_Groups/B_Type.py
+++ Instruction_Groups/B_Type.py
@@ -15,8 +15,6 @@ def formatter(ins_lst):
         else:
             bin_no = bin(int(ins_lst[-1], 16))[2:]
             fin_bin_no = '0' * (12-len(bin_no)) + bin_no
-            bin_res += fin_bin_no
-
     elif ins_lst[-1].__contains__('B'):                              # Bin immediates
         if len(ins_lst[-1][2:]) > 12:
             raise ValueError("Immediate passed is out of range!!!! \n Enter value <= 0b111111111111 \n")
@@ -24,7 +22,6 @@ def formatter(ins_lst):
         else:
             bin_no = ins_lst[-1][2:]
             fin_bin_no = '0' * (12-len(bin_no)) + bin_no
-            bin_res += fin_bin_no
     elif ins_lst[-1].isnumeric():                                    # Positive Int immediates
         if int(ins_lst[-1]) > 4095:
             raise ValueError("Immediate passed is out of range!!!! \n Enter value <= 4095 \n")
@@ -32,7 +29,6 @@ def formatter(ins_lst):
         else:
             bin_no = bin(int(ins_lst[-1]))[2:]
             fin_bin_no = '0' * (12-len(bin_no)) + bin_no
-            bin_res += fin_bin_no
     elif ins_lst[-1][1:].isnumeric() and ins_lst[-1][0] is '-':      # Negative Int immediates
         if int(ins_lst[-1][1:]) > 4094:
             raise ValueError("Immediate passed is out of range!!!! \n Enter value >= -4094 \n")
@@ -43,13 +39,12 @@ def formatter(ins_lst):
             #Flipping the bits of original number
             flipped_bits = ''.join(['1' if i == '0' else '0' for i in fin_bin_no])
             #Adding 1 to flipped bits, to get 2's complement of the negative immediate passed
-            bin_neg_no = bin(int(flipped_bits, 2) + int('1', 2))
-            bin_res += bin_neg_no[2:]        
+            fin_bin_no = bin(int(flipped_bits, 2) + int('1', 2))
     else:
         raise ValueError("Passed Immediate not allowed!!!! \n Only hex/bin/int values are allowed \n")
         return
 
-    imm = imm[-12] + imm[-10:] + imm[-11]
+    imm = fin_bin_no[-12] + fin_bin_no[-10:] + fin_bin_no[-11]
     bin_res += imm[-12:-5]
 
     if ins_lst[2].upper() in REG:
--- Instruction_Groups/J_Type.py
+++ Instruction_Groups/J_Type.py
@@ -3,34 +3,44 @@ from Registers import REG
 
 def formatter(ins_lst):
     bin_res = '0b'
-    imm = ''
     
-    if ins_lst[2].__contains__('x'):                    # Hex immediates
+    if ins_lst[2].__contains__('X'):                                    # Hex immediates
         if len(bin(int(ins_lst[2], 16))[2:]) > 20:
             raise ValueError("Immediate passed is out of range!!!! \n Enter value <= 0xfffff \n")
             return
         else:
-            imm = bin(int(ins_lst[2], 16))[2:]
-    elif ins_lst[2].__contains__('b'):                  # Bin immediates
+            bin_no = bin(int(ins_lst[-1], 16))[2:]
+            fin_bin_no = '0'*(20-len(bin_no)) + bin_no
+    elif ins_lst[2].__contains__('B'):                                  # Bin immediates
         if len(ins_lst[2][2:]) > 20:
             raise ValueError("Immediate passed is out of range!!!! \n Enter value <= 0b11111111111111111111 \n")
             return
         else:
-            imm = ins_lst[2][2:]
-    elif type(ins_lst[2]) is int:                       # Int immediates
+            bin_no = ins_lst[-1][2:]
+            fin_bin_no = '0'*(20-len(bin_no)) + bin_no
+    elif ins_lst[-1].isnumeric():                                       # Positive Int immediates
         if ins_lst[2] > 1048575:
             raise ValueError("Immediate passed is out of range!!!! \n Enter value <= 1048575 \n")
             return
         else:
-            imm = bin(ins_lst[2])[2:]
+            bin_no = bin(int(ins_lst[-1]))[2:]
+            fin_bin_no = '0'*(20-len(bin_no)) + bin_no
+    elif ins_lst[-1][1:].isnumeric() and ins_lst[-1][0] is '-':         # Negative Int immediates
+        if int(ins_lst[-1][1:]) > 1048574:
+            raise ValueError("Immediate passed is out of range!!!! \n Enter value >= -1048574 \n")
+            return
+        else:
+            bin_no = bin(int(ins_lst[-1][1:]))[2:]
+            fin_bin_no = '0' * (20-len(bin_no)) + bin_no
+            #Flipping the bits of original number
+            flipped_bits = ''.join(['1' if i == '0' else '0' for i in fin_bin_no])
+            #Adding 1 to flipped bits, to get 2's complement of the negative immediate passed
+            fin_bin_no = bin(int(flipped_bits, 2) + int('1', 2))
     else:
         raise ValueError("Passed Immediate not allowed!!!! \n Only hex/bin/int values are allowed \n")
         return
 
-    if len(imm) < 20:
-       imm = '0'*(20-len(imm)) + imm 
-
-    bin_res += imm[-20] + imm[-10:] + imm[-11] + imm[-19:-11] 
+    bin_res += fin_bin_no[-20] + fin_bin_no[-10:] + fin_bin_no[-11] + fin_bin_no[-19:-11] 
 
     if ins_lst[1].upper() in REG:
         bin_res += '0'*(7-len(bin(REG.index(ins_lst[1])))) + bin(REG.index(ins_lst[1]))[2:]
